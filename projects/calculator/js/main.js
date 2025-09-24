356e323e4647c93d4ed42e0ad6e4e3509ff1f973
Saurya Jonchhe <sauryajonchhe@Mac.lc.luther.edu>
2025-09-23 19:04:09 -0500

    Finish project 1

--- projects/calculator/js/main.js
+++ projects/calculator/js/main.js
@@ -11,19 +11,46 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    if (clearOnEntry) {
+        outputScreen.textContent = digit;
+        clearOnEntry = false;
+    } else {
+        outputScreen.textContent += digit;
+    }
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    outputScreen.textContent = "0";
+    clearOnEntry = true;
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    try {
+        const expression = outputScreen.textContent;
+        const result = eval(expression);
+        if (result == Infinity) {   // Checks for Positive Infinity
+            outputScreen.textContent = Infinity;
+        }
+        else if (isNaN(result) || !isFinite(result)) { // Checks for Not a Number and Negative Infinity
+            outputScreen.textContent = "ERROR";
+        } else {
+            outputScreen.textContent = result;
+        }
+        clearOnEntry = true;
+    } catch (error) {
+        outputScreen.textContent = "ERROR";
+        clearOnEntry = true;
+    }
+}
 
 
 /**
@@ -31,7 +58,14 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    if (clearOnEntry) {
+        outputScreen.textContent = "0" + operation;
+    } else {
+        outputScreen.textContent += operation;
+    }
+    clearOnEntry = false;
+}
 
 
 window.onload = function () {
